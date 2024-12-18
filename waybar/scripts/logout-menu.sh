2876c83720c289672bb77454266fd273a337c01b
g4mls <64618623+g4mless@users.noreply.github.com>
2024-12-18 07:09:54 +0700

    Add files via upload

---
+++ waybar/scripts/logout-menu.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+config="$HOME/.config/rofi/logout-menu.rasi"
+
+actions=$(echo -e "   Lock\n   Shutdown\n   Reboot\n $(printf '\u200A')  Suspend\n   Hibernate\n   Logout")
+
+# Display logout menu
+selected_option=$(echo -e "$actions" | rofi -dmenu -i -config "${config}")
+
+# Perform actions based on the selected option
+case "$selected_option" in
+*Lock)
+  hyprlock
+  ;;
+*Shutdown)
+  systemctl poweroff
+  ;;
+*Reboot)
+  systemctl reboot
+  ;;
+*Suspend)
+  systemctl suspend
+  ;;
+*Logout)
+  hyprctl dispatch exit 0
+  ;;
+esac
